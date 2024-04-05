8dc0737a344520c7edcd74b11c2c3ed5002fcc99
Dominique Haroun <haroundominique@gmail.com>
2024-04-05 18:15:35 +0200

    working with css and html in the same php file

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<h1>
 <?php
 echo "Hola Mundo";
 ?>
+<h1>
+
+<style>
+    :root{
+        color-scheme: light dark;
+    }
+
+    body{
+        display: grid;
+        place-content: center;
+    }
+</style>
